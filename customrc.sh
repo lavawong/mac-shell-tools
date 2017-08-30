59b64f5762579d3d49375170c7e2417723d5b7d3
LavaWong <lazy.false@gmail.com>
2017-08-30 19:57:21 +0800

    add tmux conf

--- customrc.sh
+++ customrc.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # User specific aliases and functions
+export LC_CTYPE=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
 export EDITOR="/usr/bin/vim"
 export SVN_EDITOR="/usr/bin/vim"
 # 快捷命令定义区域
@@ -14,65 +16,63 @@ alias sshtunnel="~/workspace/tools/sshtunnel.sh"
 alias g="git"
 alias gd="git diff"
 alias gs="git status"
+alias ppjson='python -mjson.tool'
+
+[ -f $HOME/.securityrc ] && source $HOME/.securityrc
 
 # $PS1 var defined
 export CLICOLOR=1
 export LSCOLORS=GxFxCxDxBxegedabagaced
+
 __ps1_last_cmd ()
 {
-    exit_code=$?
+    local exit_code=$?
     if [ $exit_code -eq "0" ]; then 
-        printf "\e[32;40m✔\e[m"
+        echo "✔"
     else 
-        printf "\e[31;40m✘\e[m"
+        echo "✘"
     fi
 
 }
 export GIT_PS1_SHOWDIRTYSTATE=1
 export GIT_PS1_SHOWCOLORHINTS=1
-[ -f $HOME/.securityrc ] && source $HOME/.securityrc
-if [ -f ~/workspace/tools/git-prompt.sh ]; then
-  source ~/workspace/tools/git-prompt.sh
-  export PS1='$(__ps1_last_cmd "(%s)") \e[33;40m\u\e[m \e[036;40m\W\e[m $(__git_ps1 "(%s)")\\$ '
-fi
-
 # findjs
 __find_js ()
 {
-	local arg1=$1
-	local arg2=$2
-	if [ -z $arg1 ] && [ -z $arg2]; then
-		echo "error: You must specify at least one parameters. [find dirpath search-expr] or [find search-expr]"
-		return 1
-	fi
-	# 'find . -name "*.html" -exec grep  {} ";"'
-	if [ -z $arg2 ]; then
-		find . -name "*.js" -type f | xargs grep -n $arg1
-	else
-		find $arg1 -name "*.js" -type f | xargs grep -n $arg2
-	fi
+    local arg1=$1
+    local arg2=$2
+    if [ -z $arg1 ] && [ -z $arg2]; then
+        echo "error: You must specify at least one parameters. [find dirpath search-expr] or [find search-expr]"
+        return 1
+    fi
+    # 'find . -name "*.html" -exec grep  {} ";"'
+    if [ -z $arg2 ]; then
+        find . -name "*.js" -type f | xargs grep -n $arg1
+    else
+        find $arg1 -name "*.js" -type f | xargs grep -n $arg2
+    fi
 }
 alias findjs=__find_js
 
 # Get lastest version
 __svnlast ()
 {
-	local path=$1
-	local number=$2
+    local path=$1
+    local number=$2
 
-	if [ -z $number ]; then
-		number=1
-	fi
+    if [ -z $number ]; then
+        number=1
+    fi
 
-	svn ls -v $path | sort -d | tail -n $number 
+    svn ls -v $path | sort -d | tail -n $number 
 }
 alias svnlast=__svnlast
 
 # Get lastest version
 __svndiff ()
 {
-	local path=$1
-	svn diff $path | colordiff
+    local path=$1
+    svn diff $path | colordiff
 }
 alias svndiff=__svndiff
 
@@ -101,7 +101,23 @@ export LESS_TERMCAP_mb=$'\E[01;31m'       # begin blinking
 export LESS_TERMCAP_md=$'\E[01;38;5;74m'  # begin bold
 export LESS_TERMCAP_me=$'\E[0m'           # end mode
 export LESS_TERMCAP_se=$'\E[0m'           # end standout-mode
-export LESS_TERMCAP_so=$'\E[38;5;246m'    # begin standout-mode - info box
+export LESS_TERMCAP_so=$'\E[30;43m'       # begin standout-mode - info box
 export LESS_TERMCAP_ue=$'\E[0m'           # end underline
 export LESS_TERMCAP_us=$'\E[04;38;5;146m' # begin underline
 
+
+
+#THIS MUST BE AT THE END OF THE FILE FOR SDKMAN TO WORK!!!
+export SDKMAN_DIR="/Users/lavawong/.sdkman"
+[[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "/Users/lavawong/.sdkman/bin/sdkman-init.sh"
+ANDROID_HOME=$HOME/Library/Android/sdk
+PATH=$PATH:$ANDROID_HOME/tools
+PATH=$PATH:$ANDROID_HOME/platform-tools
+alias mnpm="npm --registry=http://r.npm.sankuai.com \
+--cache=$HOME/.cache/mnpm \
+--disturl=http://npm.sankuai.com/dist/node \
+--userconfig=$HOME/.mnpmrc"
+
+alias catsss="lsof -i -n -P | grep privoxy"
+
+PATH=$PATH:$HOME/workspace/depot_tools
---
+++ http-forwarding.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "
+rdr pass inet proto tcp from any to any port 80 -> 127.0.0.1 port 9080
+rdr pass inet proto tcp from any to any port 443 -> 127.0.0.1 port 9443
+" | sudo pfctl -ef -
